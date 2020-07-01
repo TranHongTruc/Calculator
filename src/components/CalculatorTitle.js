623da94923b973f8c7ad6afd6d23636b1886c4f5
Truc <tranhongtruc236@gmail.com>
2020-07-01 09:46:04 +0700

    component calculatorTitle

---
+++ src/components/CalculatorTitle.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class CalculatorTitle extends Component {
+
+    render() { 
+        return ( 
+            <div className = "calculator-title">
+                {this.props.title}
+            </div>
+         );
+    }
+}
+ 
+export default CalculatorTitle;
